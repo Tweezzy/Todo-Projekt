656a99db6bb893306c904ecf245ebb056336d55c
Joshua Huck <jhuck@freicon.de>
2023-06-26 15:04:11 +0200

    second push

--- app.js
+++ app.js
@@ -1,23 +1,66 @@
 $(document).ready(function() {
 
     //AddTodo
-        var button = document.getElementById('AddButton');
+        $(document).ready(function() {
+                $('#AddButton').click(function() {
+                        addToList();
+                });
 
-        button.addEventListener('click',function () {
-        var text = document.getElementById('textfeld').value;
-        var listeElement = document.createElement('li');
-        var textnode = document.createTextNode(text);
-        listeElement.appendChild(textnode);
-        document.getElementById('liste').appendChild(listeElement);
-        document.getElementById('textfeld').value = '';
+                $('#textfeld').keyup(function(event) {
+                        if (event.keyCode === 13) { // Überprüfen, ob die gedrückte Taste die Eingabetaste ist
+                                addToList();
+                        }
+                });
 
-        button.classList.add('clicked');
-        button.innerHTML = 'Added!';
+                function addToList() {
+                        var text = $('#textfeld').val().trim();
 
-        setTimeout(function (){
-            button.classList.remove('clicked');
-            button.innerHTML = '+';
-        }, 2000);
-    });
+                        if (text !== '') {
+                                var listeElement = $('<li>').text(text);
+                                $('#liste').append(listeElement);
+                                $('#textfeld').val('');
+
+                                $('#AddButton').addClass('clicked');
+
+                                setTimeout(function() {
+                                        $('#AddButton').removeClass('clicked');
+                                }, 500);
+                        }
+                }
+        });
+
+        //Abgeschlossen
+        $(document).ready(function() {
+                $('#liste').on('click', 'li', function() {
+                        $(this).toggleClass('clicked');
+                        sortListItems();
+                });
+
+                function sortListItems() {
+                        var list = $('#liste');
+                        var listItems = list.children('li').get();
+
+                        listItems.sort(function(a, b) {
+                                var isAClicked = $(a).hasClass('clicked');
+                                var isBClicked = $(b).hasClass('clicked');
+
+                                if (isAClicked && !isBClicked) {
+                                        return -1; // a nach unten verschieben
+                                } else if (!isAClicked && isBClicked) {
+                                        return 1; // b nach unten verschieben
+                                } else {
+                                        return 0; // Reihenfolge beibehalten
+                                }
+                        });
+
+                        $.each(listItems, function(index, listItem) {
+                                list.append(listItem);
+                        });
+                }
+        });
+
+
+
+
 
 });
