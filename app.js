87e21f10bcfa87a97cbffe19036ee9391ca2e761
Andreas Schmieder <andreas.schmieder@googlemail.com>
2023-09-01 10:33:55 +0200

    fixed deleting elements

--- app.js
+++ app.js
@@ -2,162 +2,187 @@ $(document).ready(function () {
 
     var todoList = [];
 
+    var clickedID;
     var objectList = [];
-
+    // var objectToDelete;
 
     var timestamp = moment().unix();
     var Date = moment().format('MMMM Do YYYY, h:mm:ss a');
-    console.log(Date); // Ausgabe des formatierten Datums in der Konsole
-    console.log(objectList);
 
     var LocalStorageKey = "TodoListLocal";
 
     //AddTodo
-    $(document).ready(function () {
-        LoadTodo();
+    LoadTodo();
 
+    $('#AddButton').click(function () {
+        addToList();
+    });
 
-        $('#AddButton').click(function () {
+    $('#textfeld').keyup(function (event) {
+        if (event.keyCode === 13) { // Überprüfen, ob die gedrückte Taste die Eingabetaste ist
             addToList();
-        });
-
-        $('#textfeld').keyup(function (event) {
-            if (event.keyCode === 13) { // Überprüfen, ob die gedrückte Taste die Eingabetaste ist
-                addToList();
-            }
-        });
-
-        function addToList() {
-            var text = $('#textfeld').val().trim();
-            if (text === '') {
-                return;
-            }
-
-            var timestamp = moment().unix();
-            objectList.push({
-                title: text,
-                active: true,
-                created_at: timestamp,
-                changed_at: timestamp
-            });
-            $('#textfeld').val('');
-            $('#AddButton').addClass('clicked');
-            setTimeout(function () {
-                $('#AddButton').removeClass('clicked');
-            }, 500);
-            SaveTodo();
-            showTodos();
-
-        }
-
-        function showTodos() {
-            objectList.sort(function (a, b) {
-                return b.changed_at - a.changed_at;
-            });
-            $('#listeActive').empty();
-            $('#listeDone').empty();
-            console.log('Eintag aus der objectList', objectList);
-
-            var isActive = [];
-            var isDeactivated = [];
-
-
-            objectList.forEach(function (value, index) {
-                if (value.active === true) {
-                    isActive.push(value);
-                } else {
-                    isDeactivated.push(value);
-                }
-            });
-
-            console.log('isActiveList', isActive);
-            console.log('isDeactived', isDeactivated);
-
-            isActive.forEach(function (value, index) {
-                addTodoToList(value);
-            });
-
-            isDeactivated.forEach(function (value, index) {
-                addTodoToList(value);
-            });
-
-
-        }
-
-        function addTodoToList(value) {
-            var $listeElement = $('<li>').text(value.title);
-            let $button = $('<button id="openBtn">');
-            $button.append('<i class="fas fa-trash-alt"></i>');
-            $listeElement.append($button);
-            $button.hide();
-
-
-            $button.on('click', function (event) {
-                event.preventDefault();
-                $('#popup').show();
-                $('#closeBtn').on('click', function() {
-                    $('#popup').hide();
-                });
-            });
-
-            $('#yesButton').on('click', function () {
-                let index = objectList.findIndex(function (todo) {
-                    console.log('LOG index todo', todo);
-
-                    return todo.title === value.title;
-                });
-                if (index !== -1) {
-                    console.log('Index: ', index);
-                    objectList.splice(index, 1); // Entferne das Element aus der Liste
-                    SaveTodo(); // Aktualisiere den Local Storage
-                    showTodos(); // Aktualisiere die angezeigte Liste
-                }
-            });
-
-
-            $listeElement.hover(
-                function () {
-                    $button.show();
-                },
-                function () {
-                    $button.hide();
-                }
-            );
-
-            if (value.active === false) {
-                $('#listeDone').append($listeElement);
-            } else {
-                $('#listeActive').append($listeElement);
-            }
-
-            $listeElement.on('click', function () {
-                console.log('object beim click in der forEAch', value);
-                value.changed_at = moment().unix();
-                console.log('object beim click in der forEAch', value);
-                if (value.active === false) {
-                    value.active = true;
-                } else {
-                    value.active = false;
-                }
-                showTodos();
-                SaveTodo();
-
-            });
-        };
-
-        function SaveTodo() {
-            var LocalStorageValue = JSON.stringify(objectList);
-            localStorage.setItem(LocalStorageKey, LocalStorageValue);
-        }
-
-        function LoadTodo() {
-            var retrievedValue = localStorage.getItem(LocalStorageKey);
-            var retrievedArray = JSON.parse(retrievedValue);
-            if (Array.isArray(retrievedArray)) {
-                objectList = retrievedArray;
-            }
-            showTodos();
-
         }
     });
+
+    function addToList() {
+        var text = $('#textfeld').val().trim();
+        if (text === '') {
+            return;
+        }
+
+        var timestamp = moment().unix();
+        // Hier kommt noch ein Attribut zum Objekt dazu (archived). Damit kannst du dann prüfen, ob der Eintrag
+        // bereits gelöscht wurde oder nicht.
+        objectList.push({
+            id: generateUUID(),
+            title: text,
+            active: true,
+            created_at: timestamp,
+            changed_at: timestamp,
+            archived: false
+        });
+        $('#textfeld').val('');
+        $('#AddButton').addClass('clicked');
+        setTimeout(function () {
+            $('#AddButton').removeClass('clicked');
+        }, 500);
+        SaveTodo();
+        showTodos();
+
+    }
+
+    function showTodos() {
+        objectList.sort(function (a, b) {
+            return b.changed_at - a.changed_at;
+        });
+        $('#listeActive').empty();
+        $('#listeDone').empty();
+
+        var isActive = [];
+        var isDeactivated = [];
+
+        objectList.forEach(function (value, index) {
+            if (value.active === true) {
+                isActive.push(value);
+            } else {
+                isDeactivated.push(value);
+            }
+        });
+
+
+        isActive.forEach(function (value, index) {
+            addTodoToList(value);
+        });
+
+        isDeactivated.forEach(function (value, index) {
+            addTodoToList(value);
+        });
+    }
+
+    function addTodoToList(value) {
+        // Hier direkt am Anfang auf archived prüfen und einfach einen "return;" machen, falls "archived === false"
+        // Dann wird das Element nicht in die Liste übernommen.
+        if (value.archived) {
+            return;
+        }
+        var $listeElement = $('<li>').text(value.title);
+        $listeElement.data('id', value.id);
+
+        let $button = $('<button id="openBtn">');
+        $button.append('<i class="fas fa-trash-alt"></i>');
+        $listeElement.append($button);
+        $button.hide();
+
+
+        $button.on('click', function (event) {
+            clickedID = $button.parent().data('id');
+            event.stopPropagation();
+            $('#popup').show();
+
+            $('#closeBtn').on('click', function () {
+                $('#popup').hide();
+            });
+        });
+
+        // Hier liegt das Problem dass alle Einträge auf archived === true gesetzt werden. Wenn man eine ID (#yesButton)
+        // aufruft und einen click handler setzt, werden wohl sämtliche Einträge verarbeitet. Daher würde ich eher
+        // dazu raten,
+        $('#yesButton').on('click', function () {
+            objectList.forEach(entry => {
+               if (clickedID === entry.id) {
+                   entry.archived = true;
+               }
+            });
+            // Am besten hier direkt mit der variable "value" arbeiten und auf "archived = false" setzen. Dann kannst
+            // ganz einfach beim addTodoToList() auf das archived achten und die, die archived = false sind, erst gar
+            // nicht in der Liste anzeigen lassen. Die Daten bleiben dabei konsistent.
+            showTodos();
+            SaveTodo();
+            $('#popup').hide();
+
+            // let index = objectList.findIndex(function (todo) {
+            //     console.log('LOG index todo', todo);
+            //
+            //     return todo.title === value.title;
+            // });
+            // if (index !== -1) {
+            //     console.log('Index: ', index);
+            //     objectList.splice(index, 1); // Entferne das Element aus der Liste
+            //     SaveTodo(); // Aktualisiere den Local Storage
+            //     showTodos(); // Aktualisiere die angezeigte Liste
+            // }
+        });
+
+
+        $listeElement.hover(
+            function () {
+                $button.show();
+            },
+            function () {
+                $button.hide();
+            }
+        );
+
+        if (value.active === false) {
+            $('#listeDone').append($listeElement);
+        } else {
+            $('#listeActive').append($listeElement);
+        }
+
+        $listeElement.on('click', function () {
+            console.log('object beim click in der forEAch', value);
+            value.changed_at = moment().unix();
+            if (value.active === false) {
+                value.active = true;
+            } else {
+                value.active = false;
+            }
+            showTodos();
+            SaveTodo();
+
+        });
+    }
+
+    function SaveTodo() {
+        console.log('speichern der ObjectListe', objectList);
+        var LocalStorageValue = JSON.stringify(objectList);
+        localStorage.setItem(LocalStorageKey, LocalStorageValue);
+    }
+
+    function LoadTodo() {
+        var retrievedValue = localStorage.getItem(LocalStorageKey);
+        var retrievedArray = JSON.parse(retrievedValue);
+        if (Array.isArray(retrievedArray)) {
+            objectList = retrievedArray;
+        }
+        showTodos();
+    }
+
+    function generateUUID() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0,
+                v = c === 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
 });
